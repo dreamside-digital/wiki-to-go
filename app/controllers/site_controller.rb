f7a3a9c667f1dfc0edb7c2f2567347338375b60a
s-kennedy <sharon.peishan.kennedy@gmail.com>
2015-02-22 18:20:24 +0100

    joined two api calls into one. so much easier.

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -22,12 +22,12 @@ class SiteController < ApplicationController
 
 	def search_query
 		query = params[:query]
-		response = HTTParty.get('http://en.wikipedia.org/w/api.php?action=query&format=json&list=geosearch&gslimit=50&gsradius=10000&gspage=' + query)
+		response = HTTParty.get('http://en.wikipedia.org/w/api.php?action=query&format=json&generator=geosearch&ggslimit=50&ggsradius=10000&ggspage=' + query + '&prop=info&inprop=url')
 	end
 
 	def search_coords
 		location = params[:location]
-		response = HTTParty.get('http://en.wikipedia.org/w/api.php?action=query&format=json&list=geosearch&gslimit=50&gsradius=10000&gscoord=' + location)
+		response = HTTParty.get('http://en.wikipedia.org/w/api.php?action=query&format=json&generator=geosearch&ggslimit=50&ggsradius=10000&ggscoord=' + location + '&prop=info&inprop=url')
 	end
 
 	def process_response(response)
@@ -40,20 +40,9 @@ class SiteController < ApplicationController
 			render 'no_results'
 			flash[:search_error] = results["error"]["info"]
 		else
-			@results_array = results["query"]["geosearch"]
-			@full_results = get_full_wiki_info
+			@results_array = results["query"]["pages"]
 			render layout:false
 		end
-	end
-
-	def get_full_wiki_info
-		
-		results_ids = []
-		url = "http://en.wikipedia.org/w/api.php?action=query&format=json&prop=info&inprop=url&pageids="
-		@results_array.each { |result| url << result["pageid"].to_s << "|"}
-		response = HTTParty.get(url)
-		parsed_response = JSON.parse(response.body)
-		parsed_response["query"]["pages"]
 	end
  
 
