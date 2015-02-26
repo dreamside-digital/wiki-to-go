283593b3fcf6e15298945ff9e90c595e18c95199
s-kennedy <sharon.peishan.kennedy@gmail.com>
2015-02-26 15:53:39 +0100

    stupid delete icons. it doesn't work.

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -15,26 +15,40 @@ UserSelectedArticles.prototype.showList = function() {
 };
 
 UserSelectedArticles.prototype.addArticle = function(articleID) {
+  $("#intro-text").hide();
+  $("#selected-articles-list").show();
+
   var article = resultsList.filter(function(element) { 
     return element["id"] == articleID;
   })[0];
-  $("#intro-text").hide();
-  $("#selected-articles-list").show();
-  $("#selected-results").append('<li>' + article.title + '<span class="glyphicon glyphicon-remove" aria-hidden="true" id="' + articleID + '"></span></li>');
+  var newListItem = document.createElement("li");
+  var newGlyphicon = document.createElement("span");
+  var self = this
+  newGlyphicon.setAttribute("class", "glyphicon glyphicon-remove");
+  newGlyphicon.setAttribute("aria-hidden", "true");
+  newGlyphicon.setAttribute("id", articleID);
+  newGlyphicon.addEventListener('click', self.removeArticle);
+  newListItem.innerHTML = article.title;
+  newListItem.setAttribute("id", articleID);
+  $('#selected-results').append(newListItem).append(newGlyphicon);
+
   selectedArticles.push(article);
-  debugger;
-  $('.glyphicon-remove').on('click', this.removeArticle)
+  // $("#selected-results").append('<li>' + article.title + '<span class="glyphicon glyphicon-remove" aria-hidden="true" id="' + articleID + '"></span></li>');
+  // $('"#'+ articleID + '"').on('click', this.removeArticle)
 };
 
 UserSelectedArticles.prototype.removeArticle = function(event) {
-  debugger;
   var articleID = event.currentTarget.id;
   var article = resultsList.filter(function(element) { 
     return element["id"] == articleID;
   })[0];
-  $("#selected-results li:last-child").remove();
-  var index = selectedArticles.indexOf(article);
-  selectedArticles.splice(index, 1);
+  $(this).remove();
+  var index = selectedArticles.indexOf(article); 
+  if (index > -1) {
+      selectedArticles.splice(index, 1);
+  } else {
+    selectedArticles.pop();
+  }
 };
 
 UserSelectedArticles.prototype.makeBook = function() {
