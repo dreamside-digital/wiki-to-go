954cf52bd86d5fc827378454f3283643fbc34790
Sharon <sharon.peishan.kennedy@gmail.com>
2015-04-11 18:31:35 +0200

    edited url in make_book_pdf

--- app/services/article_creator.rb
+++ app/services/article_creator.rb
@@ -31,7 +31,7 @@ class ArticleCreator
       url = ('/'+ pageid +'.pdf')
       articles.each { |article| article.update_attribute(:pdf_path, url) }
     else
-      kit = PDFKit.new('https://en.wikipedia.org/?curid=' + pageid )
+      kit = PDFKit.new('http://en.wikipedia.org/?curid=' + pageid )
       file = kit.to_file('public/'+ pageid +'.pdf')
       url = ('/'+ pageid +'.pdf')
       article.update_attribute(:pdf_path, url)
@@ -46,7 +46,7 @@ class ArticleCreator
   def make_book_pdf(attrib)
     user_id = attrib[:user_id].to_s
     book_id = attrib[:id].to_s
-    url = 'users/' + user_id + '/books/' + book_id + '/preview'
+    url = 'http://www.wikitogo.co/users/' + user_id + '/books/' + book_id + '/preview'
     title = book_id
     kit = PDFKit.new(url)
     file = kit.to_file('public/'+ title +'.pdf')
