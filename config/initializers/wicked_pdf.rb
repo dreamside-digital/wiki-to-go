0edf89dee28de16fe297def6538e49d0826797d8
Sharon <sharon.peishan.kennedy@gmail.com>
2015-08-05 23:07:55 +0200

    updated wkpdf initializer for prod

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,7 @@
-WickedPdf.config = {
-  #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
-  #:layout => "pdf.html",
-  :exe_path => '/Users/sharon/.rvm/gems/ruby-2.2.0/bin/wkhtmltopdf'
-}
+WickedPdf.config do |config|  
+  if Rails.env == 'production' then
+    config.exe_path = Rails.root.to_s + "/bin/wkhtmltopdf"
+  else 
+    config.exe_path = '/Users/sharon/.rvm/gems/ruby-2.2.0/bin/wkhtmltopdf' 
+  end
+end
