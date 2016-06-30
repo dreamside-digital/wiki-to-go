245e4c3faf1749e4ed9b72afd1057518b82181dd
Sharon <sharon.peishan.kennedy@gmail.com>
2016-06-30 18:10:55 -0400

    installed devise for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 	has_secure_password
 	has_many :books
   # has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "30x30>" }, :default_url => "/images/:style/little-bird.png"
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   root 'site#index'
 
   post   "/login",  to: "sessions#create"
